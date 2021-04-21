fa3f47fbce6398a2641a374b32cdbec1922f76d3
hyeyoung <khy8111@naver.com>
2021-04-21 13:59:18 +0900

    MainVO 커밋

---
+++ MAR/src/main/java/com/sist/mar/main/domain/MainVO.java
@@ -0,0 +1,87 @@
+package com.sist.mar.main.domain;
+
+import com.sist.mar.cmn.DTO;
+
+public class MainVO extends DTO{
+	
+	private String path        ; //경로
+	private String name        ; //상품명
+	private int    discount    ; //할인율
+	private int    price       ; //가격
+	private int    finalPrice  ; //최종가격
+	private int    sales       ; //판매량
+	
+	
+	
+	public MainVO() {}
+
+	public MainVO(String path, String name, int discount, int price, int finalPrice, int sales) {
+		super();
+		this.path = path;
+		this.name = name;
+		this.discount = discount;
+		this.price = price;
+		this.finalPrice = finalPrice;
+		this.sales = sales;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getFinalPrice() {
+		return finalPrice;
+	}
+
+
+	public void setFinalPrice(int finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+
+	public int getSales() {
+		return sales;
+	}
+
+	public void setSales(int sales) {
+		this.sales = sales;
+	}
+
+
+	@Override
+	public String toString() {
+		return "MainVO [path=" + path + ", name=" + name + ", discount=" + discount + ", price=" + price
+				+ ", finalPrice=" + finalPrice + ", sales=" + sales + ", toString()=" + super.toString() + "]";
+	}
+
+	
+}
